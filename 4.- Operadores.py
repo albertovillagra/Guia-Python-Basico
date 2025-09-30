fc7055165d6ec1e6604f8d2ac41fe6f401f42ca6
albertovillagra <albertovillagra.s@gmail.com>
2025-09-30 09:17:40 -0300

    Add files via upload

---
+++ 4.- Operadores.py
@@ -0,0 +1,6 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Los operadores permiten realizar cambios, asignaciones, comparaciones de los
+datos que utilicemos con estos mismos. Hay diferentes tipos y usos para los
+mismos
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
---
+++ 4.1 Aritmeticos.py
@@ -0,0 +1,43 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Operadores aritmeticos
+
+Los operadores aritmeticos son los que permiten trabajar directamente con
+datos primitivos numericos, aun que alguno de ellos son posibles utilizar
+para datos primitivos string
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#Sirve aplicar operadores a datos directos "5+4" o entre variables "x+y"
+#Variables predefinidas para operadores aritmeticos:
+x=4 
+y=5
+'''SUMA + '''
+z=x+y #Valor de z sera 9
+
+'''RESTA - '''
+z=x-y #Valor de z sera 1
+
+'''MULTIPLICACION *'''
+z=x*y #Valor de z sera 20
+
+'''DIVISION / '''
+z=x/y #Valor de z sera 0.8
+z=y/x #Valor de z sera 1.25
+
+'''FACTORIZACION ** '''
+z=x**y #Valor de z sera 1024
+z=y**x #Valor de z sera 625
+
+'''MODULO O RESTO %'''
+#Modulo muestra el sobrante entero deu na division, antes que esta misma sea
+#utilizada para dar resultados decimales. Si el divisor es mayor que el
+#dividendo entonces modulo sera el valor del dividendo ya que es el que origina
+#el resultado decimal
+
+z=x%y #Valor de z sera 4
+z=y%x #Valor de z sera 1
+
+'''COCIENTE //'''
+#Cociente, por otro lado, muentra el valor entero resultado de las veces en que
+#el divisor cabe en el dividendo
+
+z=x//y #El valor de z sera 0
+z=y//x #El valor de z sera 1
