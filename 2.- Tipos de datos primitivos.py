eaca803478a74f874387ad6f2600c3f551d21d7a
albertovillagra <albertovillagra.s@gmail.com>
2025-10-01 03:03:39 -0300

    Add files via upload

--- 2.- Tipos de datos primitivos.py
+++ 2.- Tipos de datos primitivos.py
@@ -6,29 +6,74 @@ primitivos y los veremos asignandolos a variables
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-Numeros enteros son tipos int que es abreviacion de integer
-Numeros decimales son tipo float
-Palabras  son tipo str que es abreviacion de string, si no se formatean con
-    un tipo definido previo o durante la asignacion debe ir entre comillas
-    simples o dobles '' o ""
+Int / Integer
+
+Numeros enteros de tipo int que es abreviacion de integer
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var1=int(10) #se asigna 10 como int, no es necesario ya que con solo indicar
+             #10 es
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Float / Floating
+
+Numeros decimales son tipo float abreviacion de floating
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var2=float(3)#Se asignara 3 a un float lo que lo dejara como 3.0
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Str / String
+
+Palabras  son tipo str que es abreviacion de string
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var3=str(10) #se asigna 10 como str a pesar que no se encierra en '' 
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Bool / Boolean
+
 True o False respetando su primera en mayusculas son tipo bool, abreviacion
     de booleano que es utilizado para indicar si algo es verdadero o falso
     a veces se usan numeros como 1 y 0
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var4=bool(1) #bool tambien acepta 1 y 0, 1 True 0 False
+var5=True #Aisgnado como True, debe ir sin comillas y la T en mayusculas
+var6=False #Aisgnado como False, debe ir sin comillasla y la F en mayusculas
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-Indicando el tipo primitivo a variables
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-var1=int(10) #se asigna 10 como int, no es necesario ya que con solo indicar
-             #10 es suficiente
-var2=str(10) #se asigna 10 como str a pesar que no se encierra en '' o ""
-var3=bool(1) #bool tambien acepta 1 y 0, 1 True 0 False
-var4=float(3)#Se asignara 3 a un float lo que lo dejara como 3.0
-var5='' #tipo str pero vacio
-
-
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-Python no aceta, como js con let, que una variable sea declarada sin datos
+Python no acepta, como js con let, que una variable sea declarada sin datos
 lo que provoca un error
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-var6=
+#var7=
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Otros Tipos Primitivos no tan usados
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Bytes
+Dato primitivo que se indica con b'' sea para texto, numero, etc. pero debe
+ir entre comillas. Es utizado para que luego mediante un print del texto
+indexado de su valor ascii. No funciona con el valor completo solo indices
+y es inmutable o sea  ose puede cambiar
+
+Indiexacion lo veremos mas adelante
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var8=b"Hola" #La variable tiene asignado b"Hola" realmente
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Byte Array
+
+Funciona del a misma forma que byte pero este si es mutable
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var9=vytearray(b'Hola')
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+None
+
+Es un tipo de datos unico y que indica que su valor es nulo o none
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+var10=None
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Existen otros como complex, range, memoryview y frozenset que no se abordaran
+pero puedes investigarlos
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
