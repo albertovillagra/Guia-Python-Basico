f5e98f5f2a2d4a6388f740984fe15940132e7c04
albertovillagra <albertovillagra.s@gmail.com>
2025-10-01 18:25:35 -0300

    Add files via upload

---
+++ 4.2 Comparacion.py
@@ -0,0 +1,63 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Operadores relacionales
+
+Los operadores relacionales realizan comparaciones revisando si un
+valor es igual, mayor, menor, desigual, mayor o igual y menor o igual
+que el otro dato. Sus resultados seran siempre bool
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Igual
+
+Se utiliza con == ya que el = es de asignacion, compara si un valor es igual
+al otro
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+x=4==4 #Resultado: True
+x=4==5 #Resultado: False
+x'a'=='c' #Resultado: False ya que son diferentes
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Desigual o no igual
+
+Se utiliza con != dara True si los resultados son diferentes
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+x=4!=5 #Resultado: True ya que son desiguales
+x=4!=4 #Resultado: False ya que son iguales
+x=4!='a' #Resultado: True ya que son desiguales
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Mayor
+
+Se utiliza con > y compara si el valor izquierdo es mayor que el derecho
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+x=4>4 #Resultado: False debido a que son iguales
+x=4>5 #Resultado: False debido a que es menor
+x=4>3 #Resultado: True debido a que es mayor
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Menor
+
+Se utiliza con < y compara si el valor izquierdo es menor que el derecho
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+x=4<4 #Resultado: False debido a que son iguales
+x=4<5 #Resultado: True debido a que es menor
+x=4<3 #Resultado: False debido a que es mayor
+
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Mayor o igual
+
+Se utiliza con >= y compara si el valor izquierdo es igual o mayor al drecho
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+x=4>=6 #Resultado: False debido a que es  menor
+x=4>=4 #Resultado: True debido a que es igual
+x=4>=3 #Resultado: True debido a que es mayor
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Menor o igual
+Se utiliza con <= y compara si el valor izquierdo es igual o menor al derecho
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+x=4<=6 #Resultado: True debido a que es  menor
+x=4<=4 #Resultado: True debido a que es igual
+x=4<=3 #Resultado: False debido a que es mayor
